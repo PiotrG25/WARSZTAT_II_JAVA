36157978378268d80fd9513dd634c4c1e5a5ba5a
Piotr <piotrgoralewicz25@gmail.com>
2018-09-29 20:28:08 +0200

    przygotowanie koncepcyjne klasy Users

--- src/classes/Users.java
+++ src/classes/Users.java
@@ -6,4 +6,60 @@ public class Users {
     private String email;
     private String password;
     private int user_group;
+
+    public Users(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.setPassword(password);
+    }
+
+    public void saveToDB(){
+        //todo: czy id == 0
+        //todo: czy wszystkie atrybuty sa wypelnione
+        //todo: czy nie ma obiektu juz w taleli
+        //todo: czy zmienic istniejacy obiekt
+        //todo: czy prepare statement zwraca jakies wyjatki
+        //todo:
+    }
+
+    public Users setUserName(String username){
+        this.username = username;
+        return this;
+    }
+    public Users setEmail(String email){
+        this.email = email;
+//        todo: czy jest wyjatek zwiazany z email UNIQUEness
+//        todo: try-catch przywysylaniu do bazy zeby
+//        todo: zmienil setterem maila bo pewnie sie powtarza
+//        todo:
+        return this;
+    }
+    public Users setPassword(String password){
+//        todo: zahaszowac haslo
+//        todo:
+        this.password = "";
+        return this;
+    }
+    public Users setUser_group(int user_group){
+        this.user_group = user_group;
+//        todo: czy istnieje taka grupa
+//        todo:
+        return this;
+    }
+
+    public long getId(){
+        return id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public int getUser_group(){
+        return user_group;
+    }
 }
